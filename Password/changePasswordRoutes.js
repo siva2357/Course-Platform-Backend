db73e31635b75ab5bb89c0af85ae72fd600ff42d
siva2357 <sivakurra.ksp2357@gmail.com>
2025-09-05 13:11:41 +0530

    made chnges

--- Password/changePasswordRoutes.js
+++ Password/changePasswordRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const changePasswordController = require('./changePasswordController')
-const { identifier } = require('../middleware/identification');
+const { identifier } = require('../Middleware/identification');
 
 router.patch('/auth/instructor/:id/change-password', identifier, changePasswordController.changePassword);
 router.patch('/auth/student/:id/change-password', identifier, changePasswordController.changePassword);
